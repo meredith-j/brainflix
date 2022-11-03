d517954d6aeb3f9e44426278a086d99a9bfb6aec
Meredith Jonatan <mjonatan@me.com>
2022-11-03 13:01:26 -0400

    finish styling mobile nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import imgLogo from '../../assets/logo/BrainFlix-logo.svg';
 import App from '../../App';
 import imgUser from '../../assets/images/Mohan-muruge.jpg';
-import '../NavBar/_NavBar.scss';
+import '../NavBar/NavBar.scss';
 
 function NavBar() {
     return (
@@ -12,7 +12,7 @@ function NavBar() {
             </a>
         </section>
         <section className='nav__options'>
-            <input className="nav__search" type="text" placeholder="Search" />
+            <input className="nav__search" id="search" type="text" placeholder="Search" />
             <button className='nav__upload'>Upload</button>
         </section>
         <div className='nav__user-info'>
