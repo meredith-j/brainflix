1bd0e96951032e209e58556c31666514237a72cf
Meredith Jonatan <mjonatan@me.com>
2022-11-03 00:46:29 -0400

    add all content to js file

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,23 @@
 import imgLogo from '../../assets/logo/BrainFlix-logo.svg';
 import '../NavBar/_NavBar.scss';
 import App from '../../App';
+import imgUser from '../../assets/images/Mohan-muruge.jpg';
 
 function NavBar() {
     return (
       <>
-      <section className="nav">
-        <a className='nav__link' src={App}>
-            <img className="nav__logo" src={imgLogo}/>
-        </a>
-      </section>
-      <section className='nav__options'>
-          <input type="text" placeholder="Search.." />
-      </section>
+        <section className="nav">
+            <a className='nav__link' src={App}>
+                <img className="nav__logo" src={imgLogo}/>
+            </a>
+        </section>
+        <section className='nav__options'>
+            <input type="text" placeholder="Search.." />
+            <button className='nav__upload'>Upload</button>
+        </section>
+        <section className='nav__user-info'>
+            <img className='nav__user-photo' src={imgUser}/>
+        </section>
       </>
     );
   }
