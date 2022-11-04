b551d3e66ebb7e1b05affc5972b7dfaf255d137e
Meredith Jonatan <mjonatan@me.com>
2022-11-03 21:30:28 -0400

    add js for video details

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import './App.scss';
 import NavBar from './components/NavBar/NavBar';
 import { useState } from 'react';
 import Video from './components/Video/Video';
+import VideoInfo from './components/VideoInfo/VideoInfo';
 import videos from './data/videos.json';
+import videoDetails from './data/video-details.json';
 
 function App() {
 
-  const [ featuredVideo, setFeaturedVideo ] = useState(videos[0])
+  const [ featuredVideo, setFeaturedVideo ] = useState(videoDetails[0])
 
-  console.log(featuredVideo)
 
   return (
     <>
     <NavBar />
     <Video 
     featuredVideo = { featuredVideo }
+    />
+    <VideoInfo 
+    featuredVideo= { featuredVideo }
     />
     </>
   );
---
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import '../VideoInfo/VideoInfo.scss';
+import views from '../../assets/icons/views.svg';
+import likes from '../../assets/icons/likes.svg';
+
+function VideoInfo ( { featuredVideo } ) {
+    return (
+        <>
+            <div className='video-info'>
+                <h1 className="video-info__title">{ featuredVideo.title }</h1>
+                <section className='video-info__details'>
+                    <section className='video-info__post-info'>
+                        <p className='video-info__channel'>{ featuredVideo.channel}</p>
+                        <p className='video-info__date'>{ new Date(featuredVideo.timestamp).toLocaleDateString() }</p>
+                    </section>
+                    <section className='video-info__engagement'>
+                        <section className='video-info__views'>
+                            <img className='video-info__icon' src={views} />
+                            <p className='video-info__data'>{ featuredVideo.views }</p>
+                        </section>
+                        <section className='video-info__likes'>
+                            <img className='video-info__icon' src={likes} />
+                            <p className='video-info__data'>{ featuredVideo.likes }</p>
+                        </section>
+                    </section>
+                    <p className='video-info__description'> { featuredVideo.description } </p>
+                </section>
+            </div>
+        </>
+    )
+}
+
+ export default VideoInfo;
