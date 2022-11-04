5d70fb2ec26593f97f52badffee3b85963e94ee2
Meredith Jonatan <mjonatan@me.com>
2022-11-04 00:47:25 -0400

    add js functionality to videos

--- src/App.js
+++ src/App.js
@@ -11,8 +11,23 @@ function App() {
 
   const [ featuredVideo, setFeaturedVideo ] = useState(videoDetails[0])
 
+
+  function changeFeaturedVideo (videoId) {
+    //when an user clicks on an item, 
+    //we need to tell this function to send the object to featuredRecipe
+    // we can use the id to pass the info between the two
+    const selectedVideo =  videoDetails.find((video)=>{
+      return videoId === video.id
+    })
+    setFeaturedVideo(selectedVideo)
+  }
+
+  const removeSelectedVideo =  videos.find((video)=>{
+    return video.id === featuredVideo.id
+  })
+
   const videosCopy = [...videos]
-  //videosCopy.splice(videosCopy.indexOf(selectedVideo), 1)
+  videosCopy.splice(videosCopy.indexOf(removeSelectedVideo), 1)
 
   return (
     <>
@@ -25,6 +40,7 @@ function App() {
     />
     <NextVideos 
     videos = { videosCopy }
+    changeFeaturedVideo = { changeFeaturedVideo}
     />
     </>
   );
--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -1,14 +1,14 @@
 import './NextVideos.scss';
 import { useState } from 'react';
 
-function NextVideos ( { videos } ) {
+function NextVideos ( { videos, changeFeaturedVideo } ) {
     return (
       <>
         <section className="next-videos">
             <h3 className="next-videos__header">Next Videos</h3>
                 { videos.map((video) => {
                     return (
-                        <card className="next-videos__card">
+                        <card onClick={()=>{ return changeFeaturedVideo(video.id)}} className="next-videos__card">
                             <img className="next-videos__poster" key={ video.id } src={ video.image } alt={ video.title } />
                             <h4 className='next-videos__title'>{ video.title }</h4>
                             <p className='next-videos__channel'>{ video.channel }</p>
