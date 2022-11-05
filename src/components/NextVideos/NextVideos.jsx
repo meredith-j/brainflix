70cddea407dc2eec37b7d1362638fc928d93703a
Meredith Jonatan <mjonatan@me.com>
2022-11-05 15:36:46 -0400

    update fonts partial for mobile view

--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -6,12 +6,12 @@ function NextVideos ( { videos, changeFeaturedVideo } ) {
     return (
       <>
         <section className="next-videos">
-            <h3 className="next-videos__header">Next Videos</h3>
+            <h2 className="next-videos__header">Next Videos</h2>
                 { videos.map((video) => {
                     return (
                         <article onClick={()=>{ return changeFeaturedVideo(video.id)}} className="next-videos__card" key={ video.id }>
                             <img className="next-videos__thumbnail" key={ video.id } src={ video.image } alt={ video.title } />
-                            <h4 className='next-videos__title'>{ video.title }</h4>
+                            <h3 className='next-videos__title'>{ video.title }</h3>
                             <p className='next-videos__channel'>{ video.channel }</p>
                         </article>      
                     )
