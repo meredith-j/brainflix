afe0cf8ded9412b0bb55106c714cb42d109cc2a9
Meredith Jonatan <mjonatan@me.com>
2022-11-05 18:52:50 -0400

    style NextVideos for mobile

--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -10,9 +10,13 @@ function NextVideos ( { videos, changeFeaturedVideo } ) {
                 { videos.map((video) => {
                     return (
                         <article onClick={()=>{ return changeFeaturedVideo(video.id)}} className="next-videos__card" key={ video.id }>
-                            <img className="next-videos__thumbnail" key={ video.id } src={ video.image } alt={ video.title } />
-                            <h3 className='next-videos__title'>{ video.title }</h3>
-                            <p className='next-videos__channel'>{ video.channel }</p>
+                            <div className='next-videos__video'>
+                                <img className="next-videos__thumbnail" key={ video.id } src={ video.image } alt={ video.title } />
+                            </div>
+                            <div className='next-videos__info'>
+                                <h3 className='next-videos__title'>{ video.title }</h3>
+                                <p className='next-videos__channel'>{ video.channel }</p>
+                            </div>
                         </article>      
                     )
                 })}
