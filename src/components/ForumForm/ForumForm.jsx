65d9c6db64739650213be950013c8fb03f5cd022
Meredith Jonatan <mjonatan@me.com>
2022-11-05 17:17:30 -0400

    style ForumForm

--- src/components/ForumForm/ForumForm.jsx
+++ src/components/ForumForm/ForumForm.jsx
@@ -4,21 +4,15 @@ import imgUser from '../../assets/images/Mohan-muruge.jpg';
 function ForumForm () {
     return (
             <section className="forum-form">
-                <h2 className="forum-form__heading">Join the Conversation</h2>
                     <div className="forum-form__container">
                         <form id="newComment" className="forum-form__form">
-                            <div className="forum-form__image-container">
+                            <div className="forum-form__container">
                                 <img className="forum-form__image" src={ imgUser } alt="your profile image" />
                             </div>
-
-                            <div className="forum-form__user-container">
-                                <div className="forum-form__comment-container">
-                                    <textarea name="comment" id="comment" className="forum-form__draft" placeholder="Add a new comment"></textarea>
-                                </div>
-
-                                <div className="forum-form__submit-container">
-                                    <button className="comment__submit">Comment</button>
-                                </div>
+                            <div className="forum-form__container">
+                                <h2 className="forum-form__heading">Join the Conversation</h2>
+                                <textarea name="comment" id="comment" className="forum-form__draft" placeholder="Add a new comment"></textarea>
+                                <button className="forum-form__submit">Comment</button>
                             </div>
                         </form>
                     </div>
