93f04e4c0959f56331f7514c2626e0299a4aef28
Meredith Jonatan <mjonatan@me.com>
2022-11-08 11:34:18 -0500

    add BrowserRouter functions to HomePage and UploadPage

--- src/App.js
+++ src/App.js
@@ -1,61 +1,21 @@
 import './App.scss';
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar';
-import Video from './components/Video/Video';
-import VideoInfo from './components/VideoInfo/VideoInfo';
-import Forum from './components/Forum/Forum';
-import NextVideos from './components/NextVideos/NextVideos';
-import videos from './data/videos.json';
-import videoDetails from './data/video-details.json';
+import HomePage from './pages/HomePage/HomePage';
+import UploadPage from './pages/UploadPage/UploadPage';
 
 function App() {
 
-  // this specifies what video will be playing when users navigate to the main page
-  const [ featuredVideo, setFeaturedVideo ] = useState(videoDetails[0])
-
-  // in future sprints: we will need to add setVideoObjects to the below const
-  const [ videoObjects ] = useState(videos)
-
-  // the below functions tell the two different data files to match whichever video is featured/playing and to remove it from the next videos list
-  function changeFeaturedVideo (videoId) {
-    const selectedVideo =  videoDetails.find((video)=>{
-      return videoId === video.id
-    })
-    setFeaturedVideo(selectedVideo)
-  }
-
-  const removeSelectedVideo =  videoObjects.find((video)=>{
-    return video.id === featuredVideo.id
-  })
-
-  // this part of the function removes (splices) the featured video from the next videos list  
-  const videosCopy = [...videoObjects]
-  videosCopy.splice(videosCopy.indexOf(removeSelectedVideo), 1)
-
   return (
     <>
-    <NavBar />
-    <Video 
-    featuredVideo = { featuredVideo }
-    />
-    {/* the sections/divs here help us style our website for the desktop view -- we are using flexbox
-    to move the next videos section up beside the current video info & comments section */}
-    <section className='main__written-content'>
-    <div className='main__section'>
-    <VideoInfo 
-    featuredVideo= { featuredVideo }
-    />
-    <Forum 
-    featuredVideo = { featuredVideo }
-    />
-    </div>
-    <div className='main__sidebar'>
-    <NextVideos 
-    videos = { videosCopy }
-    changeFeaturedVideo = { changeFeaturedVideo}
-    />
-    </div>
-    </section>
+    <BrowserRouter>
+      <NavBar />
+        <Routes>
+          <Route path="/" element={ <HomePage />} />
+          <Route path="/upload" element={ <UploadPage />} />
+        </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,22 @@
 import imgLogo from '../../assets/logo/BrainFlix-logo.svg';
 import imgUser from '../../assets/images/Mohan-muruge.jpg';
+import { Link } from 'react-router-dom';
 import '../NavBar/NavBar.scss';
 
-function NavBar( { App } ) {
+function NavBar() {
     return (
       <>
         <div className='navbar'>
             <section className="navbar__heading">
               {/* this logo links to home page */}
-                <a className='navbar__link' href="/">
+                <Link to="/">
                     <img className="navbar__logo" src={imgLogo} alt="Your profile picture" />
-                </a>
+                </Link>
             </section>
             <section className='navbar__items'>
               <section className='navbar__options'>
                   <input className="navbar__search" id="search" type="text" placeholder="Search" />
-                  <button className='navbar__upload'>Upload</button>
+                  <Link to="/upload"><button className='navbar__upload'>Upload</button></Link>
               </section>
               <div className='navbar__user-info'>
                   <img className='navbar__user-photo' src={imgUser}/>
