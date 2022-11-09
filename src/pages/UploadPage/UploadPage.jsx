a41c9365923fbf8277e2fd9d08e6417a3233cd6a
Meredith Jonatan <mjonatan@me.com>
2022-11-08 20:23:38 -0500

    finish styling upload page

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,7 +1,31 @@
 import './UploadPage.scss';
+import { Link } from 'react-router-dom';
+import thumbnail from '../../assets/images/Upload-video-preview.jpg';
 
 export default function UploadPage () {
     return (
-        <p>hello!</p>
+        <section className='upload'>
+            <h1 className='upload__header'>Upload Video</h1>
+            <div className='upload__sections'>
+                <div className='upload__section upload__section--thumbnail'>
+                    <h4 className='upload__type'>Video Thumbnail</h4>
+                    <img className='upload__thumbnail' src={ thumbnail }/>
+                </div>
+                <div className='upload__form'>
+                    <div className='upload__section'>
+                        <h4 className='upload__type'>Title your video</h4>
+                        <input className="upload__title" id="title" type="text" placeholder="Add a title to your video" />
+                    </div>
+                    <div className='upload__section'>
+                        <h4 className='upload__type'>Add a video description</h4>
+                        <textarea name="description" id="description" className="upload__description" placeholder="Add a description to your video"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div className='upload__options'>
+                <button className="upload__submit">Publish</button>
+                <Link to="/"><button className="upload__cancel">Cancel</button></Link>
+            </div>
+        </section>
     )
 }
